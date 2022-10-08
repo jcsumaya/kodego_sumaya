073f3536335b63540b2eabf529edb83b2fc81cf9
jcsumaya <sumaya.jcs@gmail.com>
2022-10-08 10:10:01 +0800

    edited Activity1a.kt

--- src/main/kotlin/activity_01_a/Activity1a.kt
+++ src/main/kotlin/activity_01_a/Activity1a.kt
@@ -1,5 +1,9 @@
 package activity_01_a
+import java.util.*
+
 fun main() {
+    var className:String? = null
+    var course:String? = null
     var lastName:String? = null
     var firstName:String? = null
     var middleName:String? = null
@@ -8,4 +12,5 @@ fun main() {
     var grade:Double? = null
     var isPresent:Boolean? = null
     var isAbsent:Boolean? = null
+    var date = Date(null)
 }
