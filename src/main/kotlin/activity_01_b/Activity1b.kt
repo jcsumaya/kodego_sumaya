605c3c884da7d9fde8775bd1b284ac527ddcf411
jcsumaya <sumaya.jcs@gmail.com>
2022-10-10 16:27:14 +0800

    added Activity1b.kt

--- src/main/kotlin/activity_01_b/Activity1b.kt
+++ src/main/kotlin/activity_01_b/Activity1b.kt
@@ -1,10 +1,17 @@
 package activity_01_b
-import java.util.Date
+import java.util.*
 
 fun main() {
-    var bookTitle:String? = null
+    var bookTitle:String? = "a".uppercase(Locale.getDefault())
+        var booksAtoF = bookTitle?.first() in 'A'..'F'
+        var booksGtoL = bookTitle?.first() in 'G'..'L'
+        var booksMtoR = bookTitle?.first() in 'M'..'R'
+        var booksStoZ = bookTitle?.first() in 'S'..'Z'
     var bookGenre:String? = null
-    var bookYear:Int? = null
+    var bookYear:Int = 0
+        var bookYear2000s = bookYear in (2000..2010)
+        var bookYear2010s = bookYear in (2011..2020)
+        var bookYear2020s = bookYear >= 2021
     var bookAuthor:String? = null
     var isAvailable:Boolean? = null
     var isBorrowable:Boolean? = null
