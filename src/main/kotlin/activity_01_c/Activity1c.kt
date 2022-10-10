c7c03cff8e44da9fcdbc10d8322a524add9a1699
jcsumaya <sumaya.jcs@gmail.com>
2022-10-10 17:47:25 +0800

    edited Activity1c.kt

--- src/main/kotlin/activity_01_c/Activity1c.kt
+++ src/main/kotlin/activity_01_c/Activity1c.kt
@@ -3,21 +3,43 @@ package activity_01_c
 import java.util.Date
 
 fun main () {
-    //categories
-    var vegetable:String? = null
-    var meat:String? = null
-    var milk:String? = null
-    var beverage:String? = null
-    var bread:String? = null
-    var hygiene:String? = null
-    var cosmetic:String? = null
-    var appliances:String? = null
+    //items and categories
+    var ginger:Int? = null
+    var eggplant:Int? = null
+        var vegetable = listOf(ginger,eggplant)
+    var apple:Int? = null
+    var banana:Int? = null
+        var fruits = listOf(apple,banana)
+    var chicken:Int? = null
+    var beef:Int? = null
+        var meat = listOf(chicken,beef)
+    var cheese:Int? = null
+    var milk:Int? = null
+    var egg:Int? = null
+        var dairy = listOf(cheese,milk, egg)
+    var coffee:Int? = null
+    var juice:Int? = null
+        var beverage = listOf(coffee,juice)
+    var muffin:Int? = null
+    var milkBread:Int? = null
+        var bread = listOf(muffin,milkBread)
+    var toothBrush:Int? = null
+    var soap:Int? = null
+        var hygiene = listOf(toothBrush,soap)
+    var perfume:Int? = null
+    var lotion:Int? = null
+        var cosmetic = listOf(perfume,lotion)
+    var television:Int? = null
+    var electricFan:Int? = null
+        var appliances = listOf(television,electricFan)
 
     var date:Date? = null
     var cost:Double? = null
     var stockQuantity:Int? = null
-    var price:Double? = null
+    var weight:Double? = null
+    var itemPrice:Double? = null
     var soldQuantity:Int? = null
+    var totalPrice:Double? = null
     var isMember:Boolean? = false
 
 }
