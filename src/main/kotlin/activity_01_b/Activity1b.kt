c4307900229cc973ad56921f537c17689fd5da07
jcsumaya <sumaya.jcs@gmail.com>
2022-10-10 15:50:31 +0800

    added Activity1b.kt

---
+++ src/main/kotlin/activity_01_b/Activity1b.kt
@@ -0,0 +1,20 @@
+package activity_01_b
+
+import java.util.Date
+
+fun main() {
+    var bookTitle:String? = null
+    var bookGenre:String? = null
+    var bookYear:Int? = null
+    var bookAuthor:String? = null
+    var isAvailable:Boolean? = null
+    var isBorrowable:Boolean? = null
+    var callNumber:Int? = null
+    var studentName:String? = null
+    var studentNumber:Int? = null
+    var date:Date? = null
+    var checkIn:Date? = null
+    var checkOut:Date? = null
+    var timesOut:Int? = null
+    var lateFee:Double? = null
+}
