073b43944487852465b881ee66ec3b087cd49657
jcsumaya <sumaya.jcs@gmail.com>
2022-10-12 02:37:19 +0800

    added comment to Activity1c.kt

--- src/main/kotlin/activity_01_c/Activity1c.kt
+++ src/main/kotlin/activity_01_c/Activity1c.kt
@@ -1,7 +1,10 @@
 package activity_01_c
-
 import java.util.Date
 
+//You are tasked to automate an inventory system for a grocery.
+//Identify the items that can be bought in a grocery store.
+//After listing the different items, identify the characteristics of the items.
+//You are also tasked to group the items in categories to help manage the grocery.  Use the proper data types.
 fun main () {
     //items and categories
     var ginger:Int? = null
