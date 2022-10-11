b2c568d3f2b63f3ddf66800fa08edce85e7653f0
jcsumaya <sumaya.jcs@gmail.com>
2022-10-12 02:37:40 +0800

    added comment to Activity1a.kt

--- src/main/kotlin/activity_01_a/Activity1a.kt
+++ src/main/kotlin/activity_01_a/Activity1a.kt
@@ -1,6 +1,10 @@
 package activity_01_a
 import java.util.*
 
+//You are tasked to create a directory of the students taking this course.
+//The data will be used to send updates regarding the classes.
+//There is also a need to know the progress of the students during the progress of this course.
+//List down all data that can be used to implement this. Use the proper data types.
 fun main() {
     var className:String? = null
     var course:String? = null
