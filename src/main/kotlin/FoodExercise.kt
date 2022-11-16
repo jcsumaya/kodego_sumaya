930f92d2c6ad40495203c62a9203ca578553a9d1
jcsumaya <sumaya.jcs@gmail.com>
2022-11-16 20:27:55 +0800

    put products to ArrayList

--- src/main/kotlin/FoodExercise.kt
+++ src/main/kotlin/FoodExercise.kt
@@ -51,10 +51,10 @@ enum class OrderStatus {
     UNKNOWN
 }
 
-    class Cart(var customer: Customer){
-        var uniqueID:String = ""
-        var items:HashMap<Product, Float> = HashMap()
-        var status:OrderStatus = OrderStatus.UNKNOWN
+class Cart(var customer: Customer){
+    var uniqueID:String = ""
+    var items:HashMap<Product, Float> = HashMap()
+    var status:OrderStatus = OrderStatus.UNKNOWN
         fun updateOrder(status: OrderStatus){
             this.status = status
         }
@@ -63,39 +63,28 @@ enum class OrderStatus {
             items.put(product, quantity)
 //        items[product] = quantity
         }
-    }
+}
 
 fun main() {
-    var apple:Fruits = Fruits("Apple", 20.0)
-    var banana:Fruits = Fruits("Banana", 15.0)
-    var mango:Fruits = Fruits("Mango", 30.0)
-    var grapes:Fruits = Fruits("Grapes", 25.0)
-    var orange:Fruits = Fruits("Orange", 20.0)
+    var fruits = ArrayList<Fruits>()
+    fruits.addAll(mutableListOf(Fruits("Apple", 25.0), Fruits("Banana", 30.0),
+        Fruits("Mango", 30.0), Fruits("Grapes", 25.0), Fruits("Orange", 20.0)))
 
-    var appleShake:Shake = Shake("Apple Shake", 40.0)
-    var bananaShake:Shake = Shake("Banana Shake", 35.0)
-    var mangoShake:Shake = Shake("Mango Shake", 50.0)
-    var melonShake:Shake = Shake("Melon Shake", 45.0)
-    var orangeShake:Shake = Shake("Orange Shake", 40.0)
+    var shake = ArrayList<Shake>()
+    shake.addAll(mutableListOf(Shake("Apple Shake", 40.0), Shake("Banana Shake", 35.0),
+        Shake("Mango Shake", 50.0), Shake("Melon Shake", 45.0), Shake("Orange Shake", 40.0)))
 
-    var appleJuice:Juices = Juices("Apple Juice", 30.0)
-    var bananaJuice:Juices = Juices("Banana Juice", 25.0)
-    var mangoJuice:Juices = Juices("Mango Juice", 30.0)
-    var grapeJuice:Juices = Juices("Grape Juice", 35.0)
-    var orangeJuice:Juices = Juices("Orange Juice", 30.0)
+    var juice = ArrayList<Juices>()
+    juice.addAll(mutableListOf(Juices("Apple Juice", 30.0), Juices("Banana Juice", 25.0),
+        Juices("Mango Juice", 30.0), Juices("Grape Juice", 35.0), Juices("Orange Juice", 30.0)))
 
-    var cheese:Sandwiches = Sandwiches("Cheese", 30.0)
-    var bacon:Sandwiches = Sandwiches("Bacon", 45.0)
-    var chicken:Sandwiches = Sandwiches("Chicken", 40.0)
-    var peanut:Sandwiches = Sandwiches("Peanut", 25.0)
-    var chocolate:Sandwiches = Sandwiches("Chocolate", 30.0)
+    var sandwiches = ArrayList<Sandwiches>()
+    sandwiches.addAll(mutableListOf(Sandwiches("Cheese", 30.0), Sandwiches("Bacon", 45.0),
+        Sandwiches("Chicken", 40.0), Sandwiches("Peanut", 25.0), Sandwiches("Chocolate", 30.0)))
 
-    var caesar:Salad = Salad("Caesar", 30.0)
-    var greek:Salad = Salad("Greek", 35.0)
-    var green:Salad = Salad("Green", 40.0)
-    var tuna:Salad = Salad("Tuna", 35.0)
-    var italian:Salad = Salad("Italian", 30.0)
-
+    var salad = ArrayList<Salad>()
+    salad.addAll(mutableListOf(Salad("Caesar", 30.0), Salad("Greek", 35.0),
+        Salad("Green", 40.0), Salad("Tuna", 35.0), Salad("Italian", 30.0)))
 }
 
 data class Customer(var address:String, var mobileNumber:String,
