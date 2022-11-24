265fef9ecbc9cb87486c5afc39f3e75a00933122
jcsumaya <sumaya.jcs@gmail.com>
2022-11-24 17:11:20 +0800

    initial commit; to-be-updated

--- src/main/kotlin/activity_04_a/Activity4a.kt
+++ src/main/kotlin/activity_04_a/Activity4a.kt
@@ -2,14 +2,62 @@ package activity_04_a
 import mu.KotlinLogging
 private val logger = KotlinLogging.logger{}
 
-class Student(){
-    var firstName: String = ""
-    var lastName: String = ""
-    var nickName: String = ""
-    var id: String = ""
-    var yearEnrolled: Int = 0
+class Student(var firstName:String, var lastName:String, var nickName:String,
+              var id:String, var yearEnrolled:Int){
+}
+var students = ArrayList<Student>()
+
+fun main() {
+    students.add(Student("Armel Paul", "Tabios", "ChiefArmel", "201832D", 2018))
+    students.add(Student("Marc Polo Luis", "Fausto", "Raven", "201838A", 2017))
+    students.add(Student("Abed", "Yusop", "Abed", "201876E", 2016))
+    students.add(Student("Kuku", "Palad", "Kukuy", "201831F", 2018))
+    students.add(Student("Timothy", "Randrup", "Tims", "201899G", 2015))
+    searchStudentWildSearch()
 }
 
-fun searchStudentWildSearch(nameSearch: String){
-    TODO()
+
+
+fun searchStudentWildSearch() {
+
+do {
+    print(
+    "INPUT: \n" +
+            "1 -> WildSearch in First Name \n" +
+            "2 -> WildSearch in Last Name \n" +
+            "3 -> WildSearch in Nickname \n"
+        )
+    var option: Int? = readln().toIntOrNull()
+    var searchFor: String
+
+        when (option) {
+            1 -> {
+                print("First Name WildSearch: ")
+                searchFor = readln().uppercase()
+                for (data in students) {
+                    if (data.firstName.uppercase().contains(searchFor)) {
+                        logger.info { "First name/s found: ${data.firstName}" }
+                    }
+                }
+            }
+            2 -> {
+                print("Last Name WildSearch: ")
+                searchFor = readln().uppercase()
+                for (data in students) {
+                    if (data.lastName.uppercase().contains(searchFor)) {
+                        logger.info { "Last name/s found: ${data.lastName}" }
+                    }
+                }
+            }
+            3 -> {
+                print("Nickname WildSearch: ")
+                searchFor = readln().uppercase()
+                for (data in students) {
+                    if (data.nickName.uppercase().contains(searchFor)) {
+                        logger.info { "Nickname/s found: ${data.nickName}" }
+                    }
+                }
+            }
+        }
+ }while((option == null) || (option !in 1..3))
 }
