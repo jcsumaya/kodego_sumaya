9154d5d7a103f8d95440ce5673a025151e629fa8
Juan Carlos Sumaya <sumaya.jcs@gmail.com>
2022-11-27 18:54:05 +0800

    initial commit

--- src/main/kotlin/activity_05_a/Activity5a.kt
+++ src/main/kotlin/activity_05_a/Activity5a.kt
@@ -1,1 +1,54 @@
 package activity_05_a
+
+import java.time.Year
+import java.util.*
+import kotlin.collections.ArrayList
+
+open class Person{
+    var firstName: String = ""
+    var middleName: String = ""
+    var lastName: String = ""
+    var address: String = ""
+    var birthDate = Date()
+}
+
+open class Student: Person() {
+    var yearAdmitted: Year = Year.of(2000)
+    var id: String = ""
+}
+
+class CertificateStudent: Student() {
+    var shortCourses: ArrayList<String> = ArrayList()
+}
+
+class UnderGraduateStudent: Student() {
+    var college: String = ""
+    var degree: String = ""
+    lateinit var collegeYearStart: Year
+    var collegeYearEnd: Year? = null
+    lateinit var status: StudentStatus
+}
+
+class MasterStudent: Student() {
+    var college: String = ""
+    var degree: String = ""
+    var collegeYearStart: Year = Year.of(2000)
+    var collegeYearEnd: Year? = null
+
+}
+
+class GraduateStudent: Student() {
+    var college: String = ""
+    var degree: String = ""
+    var collegeYearStart: Year = Year.of(2000)
+    var collegeYearEnd: Year? = null
+}
+
+enum class StudentStatus {
+    LeaveOfAbsence,
+    Ongoing,
+    Graduated,
+    WillBegin,
+    Unknown,
+    Shifted
+}
