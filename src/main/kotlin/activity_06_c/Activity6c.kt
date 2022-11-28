5fdb366231006ee0cb29e1c527ab5cac87b11c6b
jcsumaya <sumaya.jcs@gmail.com>
2022-11-28 12:11:04 +0800

    initial commit; to-be-updated

--- src/main/kotlin/activity_06_c/Activity6c.kt
+++ src/main/kotlin/activity_06_c/Activity6c.kt
@@ -4,3 +4,11 @@ package activity_06_c
 //where a cart can not be checked out properly.
 //Name at least 5 scenarios.
 //Create a function that will check the cart for the exceptions.
+
+ /*
+    * Item is out of stock; stockQuantity = 0
+    * Wallet has insufficient balance; walletBalance < itemPrice
+    *
+    *
+    *
+ */
