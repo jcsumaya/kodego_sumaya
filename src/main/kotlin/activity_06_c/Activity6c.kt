f807433dce3f5fd630978a16a8adfee8c39304c3
Juan Carlos Sumaya <sumaya.jcs@gmail.com>
2022-11-28 21:28:16 +0800

    completed activity 6C

--- src/main/kotlin/activity_06_c/Activity6c.kt
+++ src/main/kotlin/activity_06_c/Activity6c.kt
@@ -13,56 +13,104 @@ package activity_06_c
     * Voucher invalid; inputVoucher !in voucherList
  */
 
+fun main() {
+    var cart = Cart()
+    var wallet = UserWallet()
+    var items: HashMap<Int, Double> = HashMap()
+    var paymentMethod = PaymentMethod()
+    var voucher = Voucher()
+
+    var sampleItem1 = Cart.Item()
+        sampleItem1.price = 2.0
+        sampleItem1.stockQuantity = 5
+        sampleItem1.reserved = false
+
+    var sampleItem2 = Cart.Item()
+        sampleItem2.price = 5.0
+        sampleItem2.stockQuantity = 2
+        sampleItem2.reserved = false
+
+    items.put(sampleItem1.stockQuantity, sampleItem1.price)
+    items.put(sampleItem2.stockQuantity, sampleItem2.price)
+
+    try{
+        cart.checkItemStatus(sampleItem1)
+    }catch (e: Exception){
+        e.printStackTrace()
+    }
+
+//    cart.checkItemStatus(sampleItem)
+//    wallet.purchase(sampleItem)
+//    paymentMethod.status(CurrentStatus.LOADING)
+//    voucher.checkValidity("takeTWO")
+}
+
+class UserWallet {
+    var balance = 20.0
+
+    class InsufficientBalance(message: String = "Wallet has insufficient balance.") : Exception(message)
+
+    fun purchase(item: Cart.Item, howMany: Int){
+        balance -= (item.price * howMany)
+            if (balance < 0.0) {
+                throw InsufficientBalance()
+            }
+        item.stockQuantity -= howMany
+            if(item.stockQuantity < 0){
+                throw Cart.UnavailableItem.ItemNoStock()
+            }
+        println("Remaining Balance: $balance")
+    }
+}
+
+class Voucher{
+    var list: ArrayList<String> = arrayListOf("ONEpercent", "takeTWO", "THREEpoints",
+        "fourMORE", "FIVEoff")
+
+    class VoucherInvalid(message: String = "Voucher input is invalid.") : Exception(message)
+
+    fun checkValidity(voucherInput: String){
+        var voucher = Voucher()
+
+        if(voucherInput !in voucher.list){
+            throw VoucherInvalid()
+        }
+    }
+}
+
+enum class CurrentStatus{
+    ONLINE,
+    OFFLINE,
+    LOADING,
+    UNKNOWN
+}
+
+class PaymentMethod{
+    class PaymentMethodUnavailable(message: String = "Payment method is not available.") : Exception(message)
+
+    fun status(status: CurrentStatus){
+        if(status == CurrentStatus.OFFLINE){
+            throw PaymentMethodUnavailable()
+        }
+    }
+
+}
+
 class Cart {
     sealed class UnavailableItem(message: String) :Exception (message){
         class ItemNoStock(message: String = "Item is out of stock.") : UnavailableItem(message)
         class ReservedItem(message: String = "Item is currently reserved.") : UnavailableItem(message)
     }
 
-    class InsufficientBalance(message: String = "Wallet has insufficient balance.") : Exception(message)
-
-    class PaymentMethodUnavailable(message: String = "Payment method is not available.") : Exception(message)
-
-    class VoucherInvalid(message: String = "Voucher input is invalid.") : Exception(message)
-
     class Item{
         var stockQuantity: Int = 0
-        var itemPrice: Double = 0.0
-        var reserved: Boolean = false
-    }
-
-    class UserWallet{
-        var balance: Double = 0.0
-    }
-
-    enum class PaymentMethodStatus{
-        ONLINE,
-        OFFLINE,
-        LOADING,
-        UNKNOWN
-    }
-
-    class Voucher{
-        var list: ArrayList<String> = ArrayList()
+        var price: Double = 0.0
+        var reserved: Boolean = true
     }
 
     fun checkItemStatus(item: Item){
-        var userWallet = UserWallet()
-        var inputVoucher = Voucher()
-
-        if(item.stockQuantity <= 0){
-            throw UnavailableItem.ItemNoStock()
-        }
-        if(!item.reserved){
+        if(item.reserved){
             throw UnavailableItem.ReservedItem()
-        }
-    }
-
-    fun checkVoucher(voucherInput: String){
-        var voucher = Voucher()
-
-        if(voucherInput !in voucher.list){
-            throw VoucherInvalid()
         }
     }
 }
