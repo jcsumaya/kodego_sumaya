d09e4cef4595030aa37051df1189260796c9c20c
Juan Carlos Sumaya <sumaya.jcs@gmail.com>
2022-12-01 17:51:39 +0800

    initial commit test.kt

---
+++ src/test/kotlin/Activity7.kt
@@ -0,0 +1,52 @@
+package activity_07
+import activity_06_c.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+internal class CartChecker {
+    /*
+ * Item is out of stock; stockQuantity <= 0
+ * Item is currently reserved; reservedItem = true
+ * Wallet has insufficient balance; walletBalance < itemPrice
+ * Payment method not available; enum class paymentMethods status = Offline
+ * Voucher invalid; inputVoucher !in voucherList
+    */
+
+    @Test
+    fun itemNoStock() {
+        assertThrows<Item.NoStock> {
+            Item().stockQuantity <= 0
+                UserWallet().purchase(Item(), 2)
+        }
+    }
+
+    @Test
+    fun itemReserved() {
+        assertThrows<Item.Reserved> {
+            Item().reserved = true
+                Item().checkStatus(Item())
+        }
+    }
+
+    @Test
+    fun paymentMethodUnavailable(){
+        assertThrows<PaymentMethod.PaymentMethodUnavailable> {
+            PaymentMethod().status(CurrentStatus.OFFLINE)
+        }
+    }
+
+    @Test
+    fun voucherInvalid(){
+        assertThrows<Voucher.VoucherInvalid> {
+            Voucher().checkValidity("asdfghjkl")
+        }
+    }
+
+//    @Test
+//    fun insufficientBalance() {
+//        assertThrows<UserWallet.InsufficientBalance> {
+//            UserWallet().purchase(Item(), 1)
+//        }
+//    }
+}
+
